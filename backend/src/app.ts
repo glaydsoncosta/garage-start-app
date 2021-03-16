e91dddfd393bc35fdf04f051e35a2315b94eee79
Glaydson Costa <glaydson@hotmail.com.br>
2021-03-16 19:07:02 -0300

    Resolving bug that was breaking image serving from "assets" folder

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,16 @@
-import 'dotenv/config'
-import cors from 'cors'
-import express from 'express'
-import router from './routes'
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import router from './routes';
+var path = require('path');
 
 const app = express()
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(router)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(router);
+// We need to make sure that "assets" dir is being served by node, so we make it available to receive GET requests
+app.use('/assets', express.static(path.join(__dirname, '..', 'assets')));
 
 export default app
